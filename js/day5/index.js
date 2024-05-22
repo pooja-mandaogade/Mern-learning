4ef2228e3ffd4e29984c49b2f2c872fab4c74120
pooja-mandaogade <poojamandaogde@gmail.com>
2024-05-22 19:02:36 +0530

    pattern

---
+++ js/day5/index.js
@@ -0,0 +1,22 @@
+// for(var i=1;i<=4;i++){
+//     let str = '';
+//     for( var j=i; j<=4;j++){
+//         str += "*";
+//     }
+//     console.log(str);
+// }
+// for(var i=2;i<=4;i++){
+//     let str = '';
+//     for( var j=1;j<=i;j++){
+//         str += "*";
+//     }
+//     console.log(str);
+// }
+for( var i=1;i<=10;i++){
+    let str = '';
+    for(var j=10;j<20;j++){
+        str +="* ";
+    }
+    console.log(str);
+}
+
