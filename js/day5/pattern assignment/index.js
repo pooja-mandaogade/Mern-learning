45b41311b73798a279529c1093ba015f73b750a4
pooja-mandaogade <poojamandaogde@gmail.com>
2024-05-23 19:10:04 +0530

    pattern assignment

---
+++ js/day5/pattern assignment/index.js
@@ -0,0 +1,15 @@
+// for( var i=1;i<=10;i++){
+//     let str = '';
+//     for(var j=10;j<20;j++){
+//         str +="*";
+//     }
+//     console.log(str);
+// }
+
+for( var i=1;i<=4;i++){
+    let str = '';
+    for(var j=1;j<4;j++){
+        str +="*";
+    }
+    console.log(str);
+}
