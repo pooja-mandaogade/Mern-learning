bf9b0324d03efb6f8bc0d522cfbf33d16efb42c9
pooja-mandaogade <poojamandaogde@gmail.com>
2024-05-28 18:54:10 +0530

    paatern assinment

--- js/day5/pattern assignment/index.js
+++ js/day5/pattern assignment/index.js
@@ -6,6 +6,34 @@
 //     console.log(str);
 // }
 
+// for( var i=1;i<=4;i++){
+//     let str = '';
+//     for(var j=1;j<4;j++){
+//         str +="*";
+//     }
+//     console.log(str);
+// }
+// for( var i = 0 ;i < 4; i++){
+//     let str = " ";
+//     for(let  j = 1;j < 5; j++){
+//         str +="* ";
+//     }
+//     console.log(str);
+// }
+// for( var i=1;i <=  4; i++){
+//     let str = "";
+//     for(var j=1;j<=i;j++){
+//         str +="* ";
+//     }
+//     console.log(str);
+// }
+// for( var i=4;i>=1;i--){
+//     let str = " ";
+//     for(var j=i;j>=1;j--){
+//         str +="* ";
+//     }
+//     console.log(str);
+// }
 for( var i=1;i<=4;i++){
     let str = '';
     for(var j=1;j<4;j++){
