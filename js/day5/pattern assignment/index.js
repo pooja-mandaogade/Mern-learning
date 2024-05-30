b95643c82d2079993749e1c02ffd91871c69f482
pooja-mandaogade <poojamandaogde@gmail.com>
2024-05-30 18:44:22 +0530

    star pattern assignment1

--- js/day5/pattern assignment/index.js
+++ js/day5/pattern assignment/index.js
@@ -34,10 +34,45 @@
 //     }
 //     console.log(str);
 // }
-for( var i=1;i<=4;i++){
-    let str = '';
-    for(var j=1;j<4;j++){
-        str +="*";
+// for( var i=1;i<=4;i++){
+//     let str = '';
+//     for(var j=1;j<4;j++){
+//         str +="*";
+//     }
+//     console.log(str)
+// }
+// for( var i=1;i<=5;i++){
+//     let str = "";
+//     for(var j=1;j<=i;j++){
+//         str +=j +" ";
+//     }
+//     console.log(str);
+// }
+// for( var i=1;i<=5;i++){
+//     let str = "";
+//     for( var k=0 ; k<i-1;k++){
+//         str +="  "
+//     }
+//     for(var j=5;j>=i;j--){
+//         str +="* ";
+//     }
+//     console.log(str);
+// }
+// num = 1;
+// for( var i=1;i<=5;i++){
+//     let str = "";
+//     for(var j=1;j<=i;j++){
+//         str += (num + " "); 
+//         num++;
+//     }
+//     console.log(str);
+// }
+num = 1;
+for( var i=1;i<=5;i++){
+    let str = "";
+    for(var j=1;j<=i;j++){
+        str += (num + " "); 
+        num+=1;
     }
     console.log(str);
 }
