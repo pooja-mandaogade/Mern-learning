464c3eca8100f05b55f2adc44031be30da3ac6e9
pooja-mandaogade <poojamandaogde@gmail.com>
2024-06-29 18:09:37 +0530

    todo assignment

--- js/day5/js/dom2 assignment/TodoDOM_552626/index.js
+++ js/day5/js/dom2 assignment/TodoDOM_552626/index.js
@@ -1,35 +1,46 @@
-var task = document.getElementById('task')
-var priority = document.querySelector('#priority')
 var form = document.querySelector('form')
+var task = document.getElementById('task')
+var priority = document.getElementById('priority')
+
 
 var tbody = document.querySelector('tbody')
+
 
 form.addEventListener('submit',function(e){
     e.preventDefault()
     let obj = {
-}
+        task : task.value,
+        priority : priority.value
+    }
+    
+    let arr = JSON.parse(localStorage.getItem('task'))
+    
+    if(arr == null)
+        arr = []
+    
+    arr.push(obj)
+    localStorage.setItem('task',JSON.stringify(arr))
 
-    var td1 = document.createElement('td')
-    var td2 = document.createElement('td')
-    var td3 = document.createElement('td')
+    // localStorage.setItem('task',JSON.stringify(arr))
+    // var td1 = document.createElement('td')
+    // var td2 = document.createElement('td')
+    // var td3 = document.createElement('td')
 
-    td1.innerText = task.value
-    td2.innerText = priority.value
-    console.log(task.value)
+    // td1.innerText = task.value 
+    // td2.innerText = priority.value
 
-
-    if(priority.value == 'High')
-         td2.style.backgroundColor = 'red'
-    else if(priority.value == 'Low')
-        td2.style.background = 'green'
-
+    // if(priority.value == 'High')
+    //     td2.style.backgroundColor = 'red'
+    // else if(priority.value == 'Low')
+    //     td2.style.backgroundColor = 'green'
 
 
-    var tr = document.createElement('tr')
+    // var tr = document.createElement('tr')
 
-    tr.append(td1,td2,td3)
+    // tr.append(td1,td2,td3)
 
-    tbody.append(tr)
+    // tbody.append(tr)
 
 
 })
+ 
